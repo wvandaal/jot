23a7fc2a3870467e55ca4ccd6233622b4e95cbd5
Willem C. van Daalen <wvandaalen@outlook.com>
2014-03-13 13:24:01 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introducing: jot
+
+Welcome to jot! Jot is a single-page application (SPA) that allows users to edit, share, and download markdown from the comfort of their browser. Jot was built with Rails 4 and Backbone.js.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jot::Application.config.secret_key_base = '27930b7db06cdb47ddbab7f250f2831a5adcaa0732c2d0dfe9d114c82834747024d25824787ba153762af48fe3682e41297a2bc124fd23bc1ac474e6c57608a8'
